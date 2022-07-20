96bd06e831de7e789f2194107fa6fe8ca4efafbb
James Kyan <james.kyan88@gmail.com>
2022-07-20 09:19:34 -0400

    commit tree 0a275e3dbe570ca018a579bb29cb80e65e07924e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } } )
+    validates :summary, length: { maximum: 250 }
+    validates_inclusion_of :category, :in => ["Fiction", "Non-Fiction"]
+    
+    validate :allowed_value?
+
+    WORDS_ALLOWED = [ /Won't Believe/, /Secret/, /Top [number]/, /Guess/]
+    
+    def allowed_value?
+        if WORDS_ALLOWED.none? { |word| word.match title }
+            errors.add(:title, "not clickbait-y enough!!")
+        end
+    end
 end
